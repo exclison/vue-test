f9300e74e35b5708926c33043a5e0b0ac4350f91
Hanyuchen <exclison@foxmail.com>
2020-12-25 17:30:47 +0800

    注释

--- tour-plat/server/routes/tripinfo.js
+++ tour-plat/server/routes/tripinfo.js
@@ -5,12 +5,17 @@ const query = require("../dbconnect/index");
 const { parseToken } = require("../util/ticket");
 const route = new Router();
 
+/*
+ *@name:/tripInfo/get-trip-info
+ *@description:获取旅行信息
+ *@date: 2020-12-25 17:29:59
+*/
 route.get("/get-trip-info", async (ctx) => {
   const info = parseToken(ctx.header.ticket);
   const { id } = info;
   const hotelSql = `SELECT hotel_id,hotel_name,room_id,room_num,m.user_id,user.id,user.name AS user_name  FROM user LEFT JOIN (SELECT hotel.hotel_id,hotel.hotel_name,room.room_id,room.room_num,room.user_id FROM hotel RIGHT JOIN room ON hotel.hotel_id=room.hotel_id) m ON user.id=m.user_id WHERE id=${id}`;
   const hotelList = await query(hotelSql);
-  
+
   const flightSql = `SELECT m.id AS user_id,m.NAME AS user_name,m.phone AS user_phone,m.sex AS user_sex,flight.* FROM (SELECT user.id,user.name,user.phone,user.sex,seat.flight_id FROM user LEFT JOIN seat ON user.id=seat.user_id WHERE user.id=${id}) m LEFT JOIN flight ON m.flight_id=flight.id`;
   const flightList = await query(flightSql);
 

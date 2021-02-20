d3075a2caa6e5d936f0d54bfda69e7b4adf7793d
Hanyuchen <exclison@foxmail.com>
2021-02-20 14:39:40 +0800

    提交

--- tour-plat/src/views/login.vue
+++ tour-plat/src/views/login.vue
@@ -83,6 +83,41 @@ export default {
         console.log(res,'gg')
       })
     },
+    //进入全屏
+    full(ele) {
+      if (ele.requestFullscreen) {
+        ele.requestFullscreen()
+      } else if (ele.mozRequestFullScreen) {
+        ele.mozRequestFullScreen()
+      } else if (ele.webkitRequestFullscreen) {
+        ele.webkitRequestFullscreen()
+      } else if (ele.msRequestFullscreen) {
+        ele.msRequestFullscreen()
+      }
+    },
+    //退出全屏
+    exitFullscreen() {
+      if (document.exitFullScreen) {
+        document.exitFullScreen()
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen()
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen()
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen()
+      }
+    },
+    //判断是否全屏
+    isFullScreen() {
+      return !!(
+        (
+          document.fullscreenElement /* 标准语法 */ ||
+          document.webkitFullscreenElement /* Chrome, Safari ， Opera 语法 */ ||
+          document.mozFullScreenElement /* Firefox 语法 */ ||
+          document.msFullscreenElement
+        ) /* IE/Edge 语法 */
+      )
+    },
     // // 音频初始化
     // audioInit() {
     //   const audio = document.getElementById('videoPlayer')

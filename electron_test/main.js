24e98c3bcd2ef7aa02352352a23766fff47e40d8
Hanyuchen <e-exclison@outlook.com>
2023-07-28 16:36:20 +0800

    提交

--- electron_test/main.js
+++ electron_test/main.js
@@ -2,7 +2,7 @@
  * @Author: Hanyuchen e-exclison@outlook.com
  * @Date: 2022-05-17 15:07:19
  * @LastEditors: Hanyuchen e-exclison@outlook.com
- * @LastEditTime: 2022-10-20 10:23:38
+ * @LastEditTime: 2023-07-27 14:14:30
  * @FilePath: \vue-test\electron_test\main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,12 +16,12 @@ const createWindow = () => {
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.resolve('./preload/index.js')
+            preload: path.join(__dirname,'./preload/index.js')
         }
     })
     const menu = Menu.buildFromTemplate([
         {
-            label: app.name,
+            label: '菜单',
             submenu: [
                 {
                     click: () => win.webContents.send('update-counter', 1),

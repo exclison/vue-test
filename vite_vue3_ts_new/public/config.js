c06ba7b142fff9bee6199005ef02a816af485e13
Hanyuchen <e-exclison@outlook.ocm>
2024-04-10 10:53:17 +0800

    提交

--- vite_vue3_ts_new/public/config.js
+++ vite_vue3_ts_new/public/config.js
@@ -1,11 +1,3 @@
-/*
- * @Author: Hanyuchen e-exclison@outlook.ocm
- * @Date: 2024-04-10 10:44:45
- * @LastEditors: Hanyuchen e-exclison@outlook.ocm
- * @LastEditTime: 2024-04-10 10:52:10
- * @FilePath: \vite_vue3_ts_new\public\config.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 
 
 const BASEURL = 'http://127.0.0.1:8090'//正式环境

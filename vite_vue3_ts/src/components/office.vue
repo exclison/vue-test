78a0af6ac2371356ad57c0cbfa65189831f0bcd2
hanyuchen <e-exclison@outlook.com>
2025-01-03 22:06:58 +0800

    echarts_svg地图

--- vite_vue3_ts/src/components/office.vue
+++ vite_vue3_ts/src/components/office.vue
@@ -29,7 +29,7 @@ export default {
           geo: {
             map: 'office',
             roam: true,
-            selectedMode: 'multiple',
+            // selectedMode: 'multiple',
             layoutCenter: ['50%', '50%'],
             layoutSize: '100%',
             tooltip: {
@@ -69,168 +69,174 @@ export default {
           this.myChart.setOption(option);
         })
 
-        this.myChart.on('geoselectchanged', function (params) {
-          console.log(params)
-          const selectedNames = params.allSelected[0].name.slice();
-          for (var i = selectedNames.length - 1; i >= 0; i--) {
-            if (that.useList.indexOf(selectedNames[i]) >= 0 || that.reserveList.indexOf(selectedNames[i]) >= 0) {
-              selectedNames.splice(i, 1);
-            }
-          }
-          console.log('selected', selectedNames,that.useList);
+        this.myChart.on('click', function (params) {
+          console.log(params.name,'iii')
+          // that.myChart.setOption(option);
+
+          // const selectedNames = params.allSelected[0].name.slice();
+          // for (var i = selectedNames.length - 1; i >= 0; i--) {
+          //   if (that.useList.indexOf(selectedNames[i]) >= 0 || that.reserveList.indexOf(selectedNames[i]) >= 0) {
+          //     selectedNames.splice(i, 1);
+          //   }
+          // }
+          // console.log('selected', selectedNames,that.useList);
         });
       }).catch(err=>{
         console.log(err)
       })
 
     },
-    // distHouseSeat(){
-    //   const svgList = this.myChart.getOption().geo[0].regions
-    //   let regions = []
-    //   svgList.forEach(item => {
-    //     regions.push(this.getRegionConfig(item.name))
-    //   })
-    //   return regions
-    // },
-    // //获取区域配置
-    // getRegionConfig(name){
-    //   if(this.isReserve(name) === 1){//使用中
-    //     if(/^F/g.test(name)){//房间样式
-    //       return {
-    //         name: name,
-    //         silent: true,
-    //         itemStyle: {
-    //           color: 'rgba(237, 125, 49,0.2)'
-    //         },
-    //         emphasis: {
-    //           itemStyle: {
-    //             color: 'rgba(237, 125, 49,0.2)'
-    //           }
-    //         },
-    //         select: {
-    //           itemStyle: {
-    //             color: 'rgba(237, 125, 49,0.2)'
-    //           }
-    //         }
-    //       }
-    //     }else{//工位样式
-    //       return {
-    //         name: name,
-    //         silent: true,
-    //         itemStyle: {
-    //           color: '#ed7d31'
-    //         },
-    //         emphasis: {
-    //           itemStyle: {
-    //             color: '#ed7d31'
-    //           }
-    //         },
-    //         select: {
-    //           itemStyle: {
-    //             color: '#ed7d31'
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }else if(this.isReserve(name) === 2){//已预定
-    //     if(/^F/g.test(name)){//房间样式
-    //       return {
-    //         name: name,
-    //         silent: true,
-    //         itemStyle: {
-    //           color: 'rgba(112, 173, 71,0.2)'
-    //         },
-    //         emphasis: {
-    //           itemStyle: {
-    //             color: 'rgba(112, 173, 71,0.2)'
-    //           }
-    //         },
-    //         select: {
-    //           itemStyle: {
-    //             color: 'rgba(112, 173, 71,0.2)'
-    //           }
-    //         }
-    //       }
-    //     }else{//工位样式
-    //       return {
-    //         name: name,
-    //         silent: true,
-    //         itemStyle: {
-    //           color: '#70ad47'
-    //         },
-    //         emphasis: {
-    //           itemStyle: {
-    //             color: '#70ad47'
-    //           }
-    //         },
-    //         select: {
-    //           itemStyle: {
-    //             color: '#70ad47'
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }else{//未预定
-    //     if(/^F/g.test(name)){//房间样式
-    //       return {
-    //         name: name,
-    //         // itemStyle: {
-    //         //   label:false,
-    //         //   color: 'rgba(0, 140, 0,0.2)'
-    //         // },
-    //         silent: false,
-    //         emphasis: {
-    //           itemStyle: {
-    //             color: 'rgba(22, 176, 255,0.2)',
-    //             borderColor: '#16b0ff',
-    //             borderWidth: 5
-    //           }
-    //         },
-    //         select: {
-    //           itemStyle: {
-    //             color: 'rgba(22, 176, 255,0.2)',
-    //             opacity: 0.5
-    //           }
-    //         }
-    //       }
-    //     }else{//工位样式
-    //       return {
-    //         name: name,
-    //         silent: false,
-    //         itemStyle: {
-    //           label:false,
-    //           color: '#ccc',
-    //         },
-    //         emphasis: {
-    //           label:false,
-    //           itemStyle: {
-    //             color: undefined,
-    //             borderColor: '#16b0ff',
-    //             borderWidth: 5
-    //           }
-    //         },
-    //         select: {
-    //           label:false,
-    //           itemStyle: {
-    //             color: 'rgba(22, 176, 255,1)',
-    //             borderColor: '#16b0ff',
-    //             borderWidth: 5
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
-    // },
-    //是否包括预定
-    // isReserve(name){
-    //   if(this.useList.includes(name)){//是否已使用
-    //     return 1
-    //   }else if(this.reserveList.includes(name)){//是否已预定
-    //     return 2
-    //   }else{
-    //     return 3
-    //   }
-    // }
+    distHouseSeat(){
+      const svgList = this.myChart.getOption().geo[0].regions
+      let regions = []
+      svgList.forEach(item => {
+        regions.push(this.getRegionConfig(item.name))
+      })
+      console.log(regions)
+      return regions
+    },
+    //获取区域配置
+    getRegionConfig(name){
+      if(this.isReserve(name) === 1){//使用中
+        if(/^F/g.test(name)){//房间样式
+          return {
+            name: name,
+            silent: true,
+            itemStyle: {
+              color: 'rgba(237, 125, 49,0.2)'
+            },
+            emphasis: {
+              itemStyle: {
+                color: 'rgba(237, 125, 49,0.2)'
+              }
+            },
+            select: {
+              itemStyle: {
+                color: 'rgba(237, 125, 49,0.2)'
+              }
+            }
+          }
+        }else{//工位样式
+          return {
+            name: name,
+            silent: true,
+            itemStyle: {
+              color: '#ed7d31'
+            },
+            emphasis: {
+              itemStyle: {
+                color: '#ed7d31'
+              }
+            },
+            select: {
+              itemStyle: {
+                color: '#ed7d31'
+              }
+            },
+            z:1000,
+          }
+        }
+      }else if(this.isReserve(name) === 2){//已预定
+        if(/^F/g.test(name)){//房间样式
+          return {
+            name: name,
+            silent: true,
+            itemStyle: {
+              color: 'rgba(112, 173, 71,0.2)'
+            },
+            emphasis: {
+              itemStyle: {
+                color: 'rgba(112, 173, 71,0.2)'
+              }
+            },
+            select: {
+              itemStyle: {
+                color: 'rgba(112, 173, 71,0.2)'
+              }
+            }
+          }
+        }else{//工位样式
+          return {
+            name: name,
+            silent: true,
+            itemStyle: {
+              color: '#70ad47'
+            },
+            emphasis: {
+              itemStyle: {
+                color: '#70ad47'
+              }
+            },
+            select: {
+              itemStyle: {
+                color: '#70ad47'
+              }
+            },
+            z:1000,
+          }
+        }
+      }else{//未预定
+        if(/^F/g.test(name)){//房间样式
+          return {
+            name: name,
+            // itemStyle: {
+            //   label:false,
+            //   color: 'rgba(0, 140, 0,0.2)'
+            // },
+            silent: false,
+            emphasis: {
+              itemStyle: {
+                color: 'rgba(22, 176, 255,0.2)',
+                borderColor: '#16b0ff',
+                borderWidth: 5
+              }
+            },
+            select: {
+              itemStyle: {
+                color: 'rgba(22, 176, 255,0.2)',
+                opacity: 0.5
+              }
+            }
+          }
+        }else{//工位样式
+          return {
+            name: name,
+            silent: false,
+            itemStyle: {
+              label:false,
+              color: '#ccc',
+            },
+            emphasis: {
+              label:false,
+              itemStyle: {
+                color: undefined,
+                borderColor: '#16b0ff',
+                borderWidth: 5
+              }
+            },
+            select: {
+              label:false,
+              itemStyle: {
+                color: 'rgba(22, 176, 255,1)',
+                borderColor: '#16b0ff',
+                borderWidth: 5
+              }
+            },
+            z:1000,
+          }
+        }
+      }
+    },
+    // 是否包括预定
+    isReserve(name){
+      if(this.useList.includes(name)){//是否已使用
+        return 1
+      }else if(this.reserveList.includes(name)){//是否已预定
+        return 2
+      }else{
+        return 3
+      }
+    }
   }
 }
 </script>
